0a4e14b26d08d9719e9d6274495a4454743ec688
Matheus Augusto Santos Martinelli <44898398+MartinelliNTZ@users.noreply.github.com>
2022-10-31 19:50:06 -0300

    Initial commit

---
+++ home.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Titulo do site</title>
+    <link rel= "stylesheet" href = "style/estiloPadrao.css">
+    <link rel="icon" href="img/favicon.ico" type="image/x-icon" />
+
+</head>
+<body>
+    <ul><!--Lista de botoes de cabeçalho-->
+        <li><a href="index.html">Página Inicial</a></li>
+        <li><a href="home.php">Home</a></li>
+        <li><a href="index.php">Contact</a></li>
+        <li><a href="about.php">Sobre</a></li>
+    </ul>
+    <header>
+        <h1 > TÍTULO DA PAGINA</h1>
+    </header>
+    <main>
+        <div id="containerPainel">
+
+            <p>testo aqui</p>
+           
+        </div>
+
+
+
+        
+       
+    </main>
+    <footer>
+       
+    </footer>
+</body>
+</html>
