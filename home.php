680e4172b5531cd64adc20c103cd2fc7ef94a58f
Matheus Augusto Santos Martinelli <44898398+MartinelliNTZ@users.noreply.github.com>
2022-10-31 20:40:58 -0300

    add login

--- home.php
+++ home.php
@@ -8,9 +8,6 @@
     <link rel= "stylesheet" href = "style/estiloPadrao.css">
     <link rel="icon" href="img/favicon.ico" type="image/x-icon" />
 
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
-        integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
 
 </head>
 <body>
@@ -20,8 +17,8 @@
         <li><a href="index.php">Contact</a></li>
         <li><a href="about.php">Sobre</a></li>
     </ul>
-    <header>
-        <h1 > HOME PAGE</h1>
+    <header>        
+    <h1 > CONTROLE DE ESTOQUE</h1>
     </header>
     <main>
         <div id="containerPainel">
